b549a99c048d7b7d919b9e6a8ef2d5b56c84102b
Prasan RKP <prasan543e@gmail.com>
2025-12-02 21:49:16 +0530

    fixed and reWritten the schema fixed

--- server/src/model/note.modal.js
+++ server/src/model/note.modal.js
@@ -1,24 +1,27 @@
 import mongoose from "mongoose";
 
-const noteSchema = new mongoose.Schema({
-  uid: {
-    type: Number,
-    required: true,
-    unique: true,
-  },
+const noteSchema = new mongoose.Schema(
+  {
+    uid: {
+      type: String,
+      required: true,
+      unique: true,
+    },
 
-  title: {
-    type: String,
-    required: true,
-    trim: true,
-  },
+    title: {
+      type: String,
+      required: true,
+      trim: true,
+    },
 
-  content: {
-    tyep: String,
-    required: true,
-    trim: true,
+    content: {
+      type: String,
+      required: true,
+      trim: true,
+    },
   },
-}, {timestamps: true});
+  { timestamps: true }
+);
 
 const Note = mongoose.model("Note", noteSchema);
 export default Note;
