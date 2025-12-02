79e69654f0a96696ff9b673de68d02f77c47e189
Prasan RKP <prasan543e@gmail.com>
2025-12-02 21:06:49 +0530

    a backend instance created to connect backend + frontend via axios

--- client/src/lib/noteAxios.js
+++ client/src/lib/noteAxios.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
 export const axiosInstance = axios.create({
-    baseURL: "http://localhost:3008/auth",
+    baseURL: "http://localhost:3008/note",
     withCredentials: true
 })
